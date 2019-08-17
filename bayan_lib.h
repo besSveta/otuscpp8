c764c4f5320643be5c6d5c2d4f69db44e1fb850b
Svetlana <bessveta@gmail.com>
2019-08-17 16:14:41 +0300

    still error in tests

--- bayan_lib.h
+++ bayan_lib.h
@@ -19,6 +19,22 @@
 #include <boost/algorithm/hex.hpp>
 #include <boost/algorithm/string.hpp>
 
+
+
+#include <boost/program_options/cmdline.hpp>
+#include <boost/program_options/config.hpp>
+#include <boost/program_options/environment_iterator.hpp>
+#include <boost/program_options/eof_iterator.hpp>
+#include <boost/program_options/errors.hpp>
+#include <boost/program_options/option.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/positional_options.hpp>
+#include <boost/program_options/value_semantic.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/version.hpp>
+
+
 namespace bfs = boost::filesystem;
 namespace po = boost::program_options;
 class DuplicateFinderCreator;
