b7db7bfa67e54392d3631080a8eef51c0b9bbe32
Svetlana <bessveta@gmail.com>
2019-08-17 14:16:01 +0300

    still error in tests

--- bayan_test.cpp
+++ bayan_test.cpp
@@ -15,8 +15,8 @@ BOOST_AUTO_TEST_SUITE(async_test_suite)
 BOOST_AUTO_TEST_CASE(async__some_test_case)
 {
 std::vector<std::string> params;
-params.emplace_back("--scanDir C:\\test")l
-	auto finder = DuplicateFinderCreator::GetDuplicatesFinder(1, params);
+params.emplace_back("--scanDir C:\\test");
+	DuplicatesFinder finder = DuplicateFinderCreator::GetDuplicatesFinder(1, params);
 	  BOOST_REQUIRE_EQUAL(finder.scanDirs.size(), 1);
 BOOST_REQUIRE_EQUAL(finder.scanDirs[0], "C:\\test");
 
